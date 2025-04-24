9b8ceaeb1af37613a6bfb60522d24346d6e72efd
max <mwlosev@gmail.com>
2025-04-24 13:15:48 +0300

    Added custom annotation ValidPassword and custom validator class PasswordConstraintValidator

--- src/main/java/com/example/auth_service/validation/annotation/ValidPassword.java
+++ src/main/java/com/example/auth_service/validation/annotation/ValidPassword.java
@@ -1,4 +1,19 @@
 package com.example.auth_service.validation.annotation;
 
+import com.example.auth_service.validation.validator.PasswordConstraintValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = PasswordConstraintValidator.class)
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ValidPassword {
+    String message() default "Invalid password";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
 }
--- src/main/java/com/example/auth_service/validation/validator/PasswordConstraintValidator.java
+++ src/main/java/com/example/auth_service/validation/validator/PasswordConstraintValidator.java
@@ -1,4 +1,40 @@
 package com.example.auth_service.validation.validator;
 
-public class PasswordConstraintValidator {
+import com.example.auth_service.validation.annotation.ValidPassword;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.passay.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+public class PasswordConstraintValidator implements ConstraintValidator<ValidPassword, String> {
+    public static final Logger logger = LoggerFactory.getLogger(PasswordConstraintValidator.class);
+
+    @Override
+    public boolean isValid(String password, ConstraintValidatorContext context) {
+        logger.error("Password validation triggered for: {}", password);
+
+        PasswordValidator validator = new PasswordValidator(List.of(
+                new LengthRule(8, 30),
+                new CharacterRule(EnglishCharacterData.UpperCase, 1),
+                new CharacterRule(EnglishCharacterData.Digit, 1),
+                new CharacterRule(EnglishCharacterData.Special, 1),
+                new WhitespaceRule()
+        ));
+
+        RuleResult result = validator.validate(new PasswordData(password));
+
+        if (result.isValid()) {
+            return true;
+        }
+
+        context.disableDefaultConstraintViolation();
+        context.buildConstraintViolationWithTemplate(
+                String.join(", ", validator.getMessages(result))
+        ).addConstraintViolation();
+
+        return false;
+    }
 }
